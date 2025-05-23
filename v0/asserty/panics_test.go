580cc3862dd979f2497ba0afca0a9abc8dd36cea
fengdotdev <feng@vitruvio.cl>
2025-05-23 02:13:08 -0400

    ...

--- v0/asserty/panics_test.go
+++ v0/asserty/panics_test.go
@@ -68,6 +68,41 @@ func TestTrue(t *testing.T) {
 
 }
 
+func TestTrueWithMessage(t *testing.T) {
+
+	t.Run("asserty.TrueWithMessage against false", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.TrueWithMessage(false, "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "should panic")
+	})
+
+	t.Run("asserty.TrueWithMessage aganins True", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.TrueWithMessage(true, "should not panic") // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "should not panic")
+	})
+}
+
 func TestFalse(t *testing.T) {
 
 	t.Run("asserty.False against true", func(t *testing.T) {
@@ -97,6 +132,40 @@ func TestFalse(t *testing.T) {
 			}()
 
 			asserty.False(false) // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "should not panic")
+	})
+}
+
+func TestFalseWithMessage(t *testing.T) {
+	t.Run("asserty.FalseWithMessage against true", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.FalseWithMessage(true, "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "should panic")
+	})
+
+	t.Run("asserty.FalseWithMessage aganins False", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.FalseWithMessage(false, "should not panic") // should not panic
 		}()
 
 		assert.FalseWithMessage(t, didPanic, "should not panic")
@@ -171,6 +240,41 @@ func TestEqual(t *testing.T) {
 
 }
 
+func TestEqualWithMessage(t *testing.T) {
+
+	t.Run("asserty.EqualWithMessage against diferent 1", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.EqualWithMessage(obj{Name: "test"}, obj{Name: "test2"}, "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "Should panic when values are different")
+	})
+
+	t.Run("asserty.EqualWithMessage against equals 1", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.EqualWithMessage(obj{Name: "test"}, obj{Name: "test"}, "should not panic") // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "Should not panic when values are equal")
+	})
+}
+
 func TestNil(t *testing.T) {
 	t.Run("asserty.Nil against not nil", func(t *testing.T) {
 		didPanic := false
@@ -199,6 +303,42 @@ func TestNil(t *testing.T) {
 			}()
 
 			asserty.Nil(nil) // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "Should not panic when value is nil")
+	})
+
+}
+
+func TestNilWithMessage(t *testing.T) {
+
+	t.Run("asserty.NilWithMessage against not nil", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.NilWithMessage(obj{Name: "test"}, "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "Should panic when value is not nil")
+	})
+
+	t.Run("asserty.NilWithMessage against nil", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.NilWithMessage(nil, "should not panic") // should not panic
 		}()
 
 		assert.FalseWithMessage(t, didPanic, "Should not panic when value is nil")
@@ -257,6 +397,58 @@ func TestErr(t *testing.T) {
 	})
 }
 
+func TestErrWithMessage(t *testing.T) {
+
+	t.Run("asserty.ErrWithMessage against nil", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.ErrWithMessage(alwaysNilErr(), "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "Should panic when value is nil")
+	})
+
+	t.Run("asserty.ErrWithMessage against some error", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.ErrWithMessage(alwaysError(), "should not panic") // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "Should not panic when value is not nil")
+	})
+
+	t.Run("asserty.ErrWithMessage against fmt err", func(t *testing.T) {
+
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.ErrWithMessage(fmt.Errorf("some err"), "should not panic") // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "Should not panic when value is not nil")
+	})
+}
+
 func TestNoError(t *testing.T) {
 
 	t.Run("asserty.NoError against some error", func(t *testing.T) {
@@ -307,4 +499,55 @@ func TestNoError(t *testing.T) {
 		assert.TrueWithMessage(t, didPanic, "Should panic when value is not nil")
 	})
 
+}
+
+func TestNoErrorWithMessage(t *testing.T) {
+	t.Run("asserty.NoErrorWithMessage against some error", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.NoErrorWithMessage(alwaysError(), "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "Should panic when value is not nil")
+	})
+
+	t.Run("asserty.NoErrorWithMessage against nil", func(t *testing.T) {
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.NoErrorWithMessage(alwaysNilErr(), "should not panic") // should not panic
+		}()
+
+		assert.FalseWithMessage(t, didPanic, "Should not panic when value is nil")
+	})
+
+	t.Run("asserty.NoErrorWithMessage against fmt err", func(t *testing.T) {
+
+		didPanic := false
+
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					didPanic = true
+				}
+			}()
+
+			asserty.NoErrorWithMessage(fmt.Errorf("some err"), "should panic") // should panic
+		}()
+
+		assert.TrueWithMessage(t, didPanic, "Should panic when value is not nil")
+	})
 }
